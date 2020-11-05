8570875fee8ac6c5a95c7f9ad644f3d744b0a317
Justice London <jlondon@syrussystems.com>
2020-11-05 04:13:40 -0800

    File mode to string

--- manifests/user.pp
+++ manifests/user.pp
@@ -1,7 +1,7 @@
 define s3sync::user ( $user, $aws_key_id, $aws_secret_key, $gpg_passphrase = undef, $path ) {
   file { "$user s3cfg":
     path     => "${path}/.s3cfg",
-    mode     => 0700,
+    mode     => '0700',
     owner    => $user,
     content  => template('s3sync/s3cmd-cfg.erb'),
   }
